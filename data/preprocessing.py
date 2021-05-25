9ef024ae1fa53f3d95ce99e8167742dee5ccb81c
Sunkyoung <sunk10926@gmail.com>
2021-05-26 01:02:19 +0900

    feat: add convert example to feature

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -23,12 +23,11 @@ class InputExample(object):
 class InputFeatures(object):
     """A single set of features of data."""
     
-    def __init__(self, input_ids, input_mask, segment_ids, label_id, sentence_id=None):
+    def __init__(self, input_ids, input_mask, segment_ids, label_id):
         self.input_ids = input_ids
         self.input_mask = input_mask
         self.segment_ids = segment_ids
         self.label_id = label_id
-
 
 class DataProcessor(object):
     """Base class for data converters for sequence classification data sets."""
@@ -65,3 +64,72 @@ class NsmcProcessor(DataProcessor):
             examples.append(
                 InputExample(guid = d[0], label = d[2], first_sequence = d[1]))
         return examples
+    
+def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    label_map = {label : i for i, label in enumerate(label_list)}
+    features = []
+
+    for example in examples:
+        first_tokens = tokenizer.tokenize     
+        second_tokens = None
+        if example.second_sequence:
+            second_tokens = tokenizer.tokenize(second_tokens)        
+
+            # Modifies `first_tokens` and `second_tokens` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            _truncate_seq_pair(first_tokens, second_tokens, max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(first_tokens) > max_seq_length - 2:
+                first_tokens = first_tokens[:(max_seq_length-2)]
+
+        tokens = ["[CLS]"] + first_tokens + ["[SEP]"]
+        segment_ids = [0] * len(tokens)
+
+        if second_tokens:
+            tokens += second_tokens + ["[SEP]"]
+            segment_ids += [1] * (len(second_tokens) + 1)
+
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only real
+        # tokens are attended to.
+        input_mask = [1] * len(input_ids)
+
+        # Zero-pad up to the sequence length.
+        padding = [0] * (max_seq_length - len(input_ids))
+        input_ids += padding
+        input_mask += padding
+        segment_ids += padding
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+
+        label_id = label_map[example.label]
+        features.append(
+                InputFeatures(input_ids=input_ids,
+                              input_mask=input_mask,
+                              segment_ids=segment_ids,
+                              label_id=label_id))
+    return features
+
+
+def _truncate_seq_pair(first_tokens, second_tokens, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(first_tokens) + len(second_tokens)
+        if total_length <= max_length:
+            break
+        if len(first_tokens) > len(second_tokens):
+            first_tokens.pop()
+        else:
+            second_tokens.pop()
