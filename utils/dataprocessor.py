5a08881a505a057d7d2e0f4086cf35e760398db0
Sunkyoung <sunk10926@gmail.com>
2021-06-02 00:29:51 +0900

    refactor: override function of convert_to_feature

--- utils/dataprocessor.py
+++ utils/dataprocessor.py
@@ -1,11 +1,11 @@
 import argparse
 import json
 import os
-from typing import List, Optional
-
 import torch
-from torch.utils.data import TensorDataset
 
+from torch.utils.data import TensorDataset
+from Overrides import overrides
+from typing import List, Optional
 
 class InputExample(object):
     """A single example"""
@@ -46,6 +46,88 @@ class DataProcessor(object):
         """Gets the list of labels for this data set."""
         raise NotImplementedError()
 
+    def _convert_examples_to_features(
+        self,
+        examples: List[InputExample],
+        label_list: List[str],
+        max_seq_length: int,
+        tokenizer,
+        do_whitespace_tokenize
+    ) -> TensorDataset:
+
+        """Loads a data file into a list of `InputBatch`s."""
+        features = []
+        label_map = {label: i for i, label in enumerate(label_list)}
+
+        for example in examples:
+            if do_whitespace_tokenize:
+                first_tokens = example.first_seq.strip().split()
+                second_tokens = example.second_seq.strip().split()
+            else:
+                first_tokens = tokenizer.tokenize(example.first_seq)
+                second_tokens = tokenizer.tokenize(example.second_seq)
+
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            self._truncate_seq_pair(first_tokens, second_tokens, max_seq_length - 3)
+
+            tokens = ["[CLS]"] + first_tokens + ["[SEP]"]
+            segment_ids = [0] * len(tokens)
+            tokens += second_tokens + ["[SEP]"]
+            segment_ids += [1] * (len(second_tokens) + 1)
+
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            input_mask = [1] * len(input_ids)
+
+            # Zero-pad up to the sequence length.
+            padding = [0] * (max_seq_length - len(input_ids))
+            input_ids += padding
+            input_mask += padding
+            segment_ids += padding
+              
+            assert len(input_ids) == max_seq_length
+            assert len(input_mask) == max_seq_length
+            assert len(segment_ids) == max_seq_length
+
+            label_id = label_map[example.label]
+
+            features.append(
+                InputFeatures(
+                    input_ids=input_ids,
+                    input_mask=input_mask,
+                    label_id=label_id,
+                    segment_ids=segment_ids
+                )
+            )
+
+        return self._convert_to_tensordata(features)
+
+    def _truncate_seq_pair(
+        self, first_tokens: List[str], second_tokens: List[str], max_length: int
+    ):
+        """Truncates a sequence pair in place to the maximum length."""
+        while True:
+            total_length = len(first_tokens) + len(second_tokens)
+            if total_length <= max_length:
+                break
+            if len(first_tokens) > len(second_tokens):
+                first_tokens.pop()
+            else:
+                second_tokens.pop()
+
+    def _convert_to_tensordata(self, features: List[InputFeatures]) -> TensorDataset:
+        """ Convert to Tensors and build dataset """
+        all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+        all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+        all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
+
+        return TensorDataset(
+            all_input_ids, all_input_mask, all_segment_ids, all_label_ids
+        )
+
     @classmethod
     def _read_txt(cls, input_file: str):
         """Reads a tab separated value file."""
@@ -65,7 +147,7 @@ class DataProcessor(object):
             for rd in raw_data:
                 data.append([rd["guid"], rd["title"], rd["label"]])
             return data
-
+    
 
 class NsmcProcessor(DataProcessor):
     def get_dev_data(self, data_dir: str) -> TensorDataset:
@@ -85,7 +167,7 @@ class NsmcProcessor(DataProcessor):
 
 
 class YnatProcessor(DataProcessor):
-    def __init__(self, args, tokenizer = None):
+    def __init__(self, args, tokenizer):
         super().__init__()
         self.args = args
         self.tokenizer = tokenizer
@@ -112,16 +194,18 @@ class YnatProcessor(DataProcessor):
         for d in data:
             guid, first_seq, label = d
             examples.append(InputExample(guid, first_seq, label))
-        return self._convert_examples_to_features(
-            examples, self.get_labels(), self.args.max_seq_length, self.tokenizer
+        return self.convert_examples_to_features(
+            examples, self.get_labels(), self.args.max_seq_length, self.tokenizer, self.args.whitespace_tokenize
         )
 
+    @overrides
     def _convert_examples_to_features(
         self,
         examples: List[InputExample],
         label_list: List[str],
         max_seq_length: int,
-        tokenizer = None,
+        tokenizer,
+        do_whitespace_tokenize
     ) -> TensorDataset:
 
         """Loads a data file into a list of `InputBatch`s."""
@@ -129,29 +213,18 @@ class YnatProcessor(DataProcessor):
         label_map = {label: i for i, label in enumerate(label_list)}
 
         for example in examples:
-            if tokenizer:
-                first_tokens = tokenizer.tokenize(example.first_seq)
-            else:  # white space 기준 만으로 token화
+            if do_whitespace_tokenize:
                 first_tokens = example.first_seq.strip().split()
+            else:  # white space 기준 만으로 token화
+                first_tokens = tokenizer.tokenize(example.first_seq)
 
-            if example.second_seq:
-                second_tokens = tokenizer.tokenize(example.second_seq)
-                # Account for [CLS], [SEP], [SEP] with "- 3"
-                self._truncate_seq_pair(first_tokens, second_tokens, max_seq_length - 3)
-            else:
-                # Account for [CLS] and [SEP] with "- 2"
-                if len(first_tokens) > max_seq_length - 2:
-                    first_tokens = first_tokens[: (max_seq_length - 2)]
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(first_tokens) > max_seq_length - 2:
+                first_tokens = first_tokens[: (max_seq_length - 2)]
 
             tokens = ["[CLS]"] + first_tokens + ["[SEP]"]
 
-            if second_tokens:
-                segment_ids = [0] * len(tokens)
-                tokens += second_tokens + ["[SEP]"]
-                segment_ids += [1] * (len(second_tokens) + 1)
-
             input_ids = tokenizer.convert_tokens_to_ids(tokens)
-
             # The mask has 1 for real tokens and 0 for padding tokens. Only real
             # tokens are attended to.
             input_mask = [1] * len(input_ids)
@@ -160,9 +233,7 @@ class YnatProcessor(DataProcessor):
             padding = [0] * (max_seq_length - len(input_ids))
             input_ids += padding
             input_mask += padding
-            if second_tokens:
-              segment_ids += padding
-              assert len(segment_ids) == max_seq_length
+
             assert len(input_ids) == max_seq_length
             assert len(input_mask) == max_seq_length
             
@@ -174,38 +245,21 @@ class YnatProcessor(DataProcessor):
                     input_ids=input_ids,
                     input_mask=input_mask,
                     label_id=label_id,
-                    segment_ids=segment_ids if segment_ids else None
+                    segment_ids=None
                 )
             )
 
         return self._convert_to_tensordata(features)
 
-    def _truncate_seq_pair(
-        self, first_tokens: List[str], second_tokens: List[str], max_length: int
-    ):
-        """Truncates a sequence pair in place to the maximum length."""
-        while True:
-            total_length = len(first_tokens) + len(second_tokens)
-            if total_length <= max_length:
-                break
-            if len(first_tokens) > len(second_tokens):
-                first_tokens.pop()
-            else:
-                second_tokens.pop()
-
+    @overrides
     def _convert_to_tensordata(self, features: List[InputFeatures]) -> TensorDataset:
         """ Convert to Tensors and build dataset """
         all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-        all_input_mask = torch.tensor(
-            [f.input_mask for f in features], dtype=torch.long
-        )
-        all_segment_ids = torch.tensor(
-            [f.segment_ids for f in features], dtype=torch.long
-        )
-        all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
+        all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+        all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.float)
 
         return TensorDataset(
-            all_input_ids, all_input_mask, all_segment_ids, all_label_ids
+            all_input_ids, all_input_mask, all_label_ids
         )
 
     @staticmethod
@@ -213,11 +267,15 @@ class YnatProcessor(DataProcessor):
         parser: argparse.ArgumentParser, root_dir: str
     ) -> argparse.ArgumentParser:
         parser.add_argument(
-            "--max_seq_length",
+            "--max-seq-length",
             default=128,
             type=int,
             help="The maximum total input sequence length after WordPiece tokenization. Sequences "
             "longer than this will be truncated, and sequences shorter than this will be padded.",
+        )
+        parser.add_argument(
+            "--do-whitespace-tokenize",
+            action='store_true'
         )
 
         return parser
