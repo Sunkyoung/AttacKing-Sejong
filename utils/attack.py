e104d71eae68061c3821832a32390f40865e7226
Sunkyoung <sunk10926@gmail.com>
2021-06-02 17:26:33 +0900

    refactor: simplify getting important score

--- utils/attack.py
+++ utils/attack.py
@@ -1,6 +1,7 @@
-import torch
 from enum import Enum
 from typing import List
+
+import torch
 
 # feature.success = 1 : exceed ratio of word change
 # feature.success = 2 : Fail to attack
@@ -14,88 +15,116 @@ class SuccessIndicator(Enum):
     attack_success = 4
 
 
-class OutputFeatures(object):
-    def __init__(self, label_id: int, first_seq: str, second_seq: str = None):
-        self.label_id = label_id
-        self.first_seq = first_seq
-        self.second_seq = second_seq
-        self.final_text = None
-        self.query_length = 0
-        self.num_changes = 0
-        self.success = SuccessIndicator()
-        self.similarity = 0.0
-        self.changes = []
-
-def _get_masked(words: List[str]):
+def get_masked(words: List[str]):
     len_text = len(words)
     masked_words = []
     for i in range(len_text - 1):
-        masked_words.append(words[0:i] + ['[MASK]'] + words[i + 1:])
+        masked_words.append(words[0:i] + ["[MASK]"] + words[i + 1 :])
     # list of words
     return masked_words
 
-def get_important_scores(words, tgt_model, orig_prob, orig_label, orig_probs, tokenizer, batch_size, max_length):
-    masked_words = _get_masked(words)
-    texts = [' '.join(words) for words in masked_words]  # list of text of masked words
-    all_input_ids = []
-    all_masks = []
-    all_segs = []
-    # Put 0 padding to input for all texts
-    for text in texts:
-        inputs = tokenizer.encode_plus(text, None, add_special_tokens=True, max_length=max_length, )
-        input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
-        attention_mask = [1] * len(input_ids)
-        padding_length = max_length - len(input_ids)
-        input_ids = input_ids + (padding_length * [0])
-        token_type_ids = token_type_ids + (padding_length * [0])
-        attention_mask = attention_mask + (padding_length * [0])
-        all_input_ids.append(input_ids)
-        all_masks.append(attention_mask)
-        all_segs.append(token_type_ids)
-    seqs = torch.tensor(all_input_ids, dtype=torch.long)
-    masks = torch.tensor(all_masks, dtype=torch.long)
-    segs = torch.tensor(all_segs, dtype=torch.long)
-    seqs = seqs.to('cuda')
 
-    eval_data = TensorDataset(seqs)
-    # Run prediction for full data
-    eval_sampler = SequentialSampler(eval_data)
-    eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=batch_size)
+
+def get_important_scores(processor, target_features, tgt_model, orig_prob, orig_label, orig_probs, batch_size):
+    # masked_words = _get_masked(words)
+    # texts = [" ".join(words) for words in masked_words]  # list of text of masked words
+    # all_input_ids = []
+    # all_masks = []
+    # all_segs = []
+    # # Put 0 padding to input for all texts
+    # for text in texts:
+    #     inputs = tokenizer.encode_plus(
+    #         text,
+    #         None,
+    #         add_special_tokens=True,
+    #         max_length=max_length,
+    #     )
+    #     input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
+    #     attention_mask = [1] * len(input_ids)
+    #     padding_length = max_length - len(input_ids)
+    #     input_ids = input_ids + (padding_length * [0])
+    #     token_type_ids = token_type_ids + (padding_length * [0])
+    #     attention_mask = attention_mask + (padding_length * [0])
+    #     all_input_ids.append(input_ids)
+    #     all_masks.append(attention_mask)
+    #     all_segs.append(token_type_ids)
+    # seqs = torch.tensor(all_input_ids, dtype=torch.long)
+    # masks = torch.tensor(all_masks, dtype=torch.long)
+    # segs = torch.tensor(all_segs, dtype=torch.long)
+    # seqs = seqs.to("cuda")
+
+    masked_features = processor._get_masked(target_features)
+    eval_sampler = SequentialSampler(masked_features)
+    eval_dataloader = DataLoader(masked_features, sampler=eval_sampler, batch_size=batch_size)
+
+    # eval_data = TensorDataset(seqs)
+    # # Run prediction for full data
+    # eval_sampler = SequentialSampler(eval_data)
+    # eval_dataloader = DataLoader(eval_data, sampler=eval_sampler, batch_size=batch_size)
     leave_1_probs = []
     with torch.no_grad():
         for batch in eval_dataloader:
-            masked_input, = batch
-            bs = masked_input.size(0)
-
+            masked_input, _, _ = batch
+            # bs = masked_input.size(0)
             leave_1_prob_batch = tgt_model(masked_input)[0]  # B num-label
             leave_1_probs.append(leave_1_prob_batch)
     leave_1_probs = torch.cat(leave_1_probs, dim=0)  # words, num-label
     leave_1_probs = torch.softmax(leave_1_probs, -1)  #
     leave_1_probs_argmax = torch.argmax(leave_1_probs, dim=-1)
-    import_scores = (orig_prob
-                     - leave_1_probs[:, orig_label] #Difference between original logit output and 1 masked logit output
-                     + # Add score which In case the results change.
-                     (leave_1_probs_argmax != orig_label).float()
-                     * (leave_1_probs.max(dim=-1)[0] - torch.index_select(orig_probs, 0, leave_1_probs_argmax))
-                     ).data.cpu().numpy()
+    import_scores = (
+        (
+            orig_prob
+            - leave_1_probs[
+                :, orig_label
+            ]  # Difference between original logit output and 1 masked logit output
+            + (  # Add score which In case the results change.
+                leave_1_probs_argmax != orig_label
+            ).float()
+            * (
+                leave_1_probs.max(dim=-1)[0]
+                - torch.index_select(orig_probs, 0, leave_1_probs_argmax)
+            )
+        )
+        .data.cpu()
+        .numpy()
+    )
 
     return import_scores
 
-def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=512, cos_mat=None, w2i={}, i2w={}, use_bpe=1, threshold_pred_score=0.3):
+
+def attack(
+    feature,
+    tgt_model,
+    mlm_model,
+    tokenizer,
+    k,
+    batch_size,
+    max_length=512,
+    cos_mat=None,
+    w2i={},
+    i2w={},
+    use_bpe=1,
+    threshold_pred_score=0.3,
+):
     # MLM-process
-    words, sub_words, keys = _tokenize(feature.seq, tokenizer)
-    words = sequence.tokenize()
-    morphemes = tokenizer.tokenize()
 
     # original label
-    inputs = tokenizer.encode_plus(feature.seq, None, add_special_tokens=True, max_length=max_length, )
-    input_ids, token_type_ids = torch.tensor(inputs["input_ids"]), torch.tensor(inputs["token_type_ids"])
+    inputs = tokenizer.encode_plus(
+        feature.seq,
+        None,
+        add_special_tokens=True,
+        max_length=max_length,
+    )
+    input_ids, token_type_ids = torch.tensor(inputs["input_ids"]), torch.tensor(
+        inputs["token_type_ids"]
+    )
     attention_mask = torch.tensor([1] * len(input_ids))
     seq_len = input_ids.size(0)
-    orig_probs = tgt_model(input_ids.unsqueeze(0).to('cuda'),
-                           attention_mask.unsqueeze(0).to('cuda'),
-                           token_type_ids.unsqueeze(0).to('cuda')
-                           )[0].squeeze()
+    orig_probs = tgt_model(
+        input_ids.unsqueeze(0).to("cuda"),
+        attention_mask.unsqueeze(0).to("cuda"),
+        token_type_ids.unsqueeze(0).to("cuda"),
+    )[0].squeeze()
     orig_probs = torch.softmax(orig_probs, -1)
     orig_label = torch.argmax(orig_probs)
     current_prob = orig_probs.max()
@@ -104,26 +133,39 @@ def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=5
         feature.success = 3
         return feature
 
-    sub_words = ['[CLS]'] + sub_words[:max_length - 2] + ['[SEP]']
+    sub_words = ["[CLS]"] + sub_words[: max_length - 2] + ["[SEP]"]
     input_ids_ = torch.tensor([tokenizer.convert_tokens_to_ids(sub_words)])
-    word_predictions = mlm_model(input_ids_.to('cuda'))[0].squeeze()  # seq-len(sub) vocab.  
-    word_pred_scores_all, word_predictions = torch.topk(word_predictions, k, -1)  # seq-len k  #top k prediction 
+    word_predictions = mlm_model(input_ids_.to("cuda"))[
+        0
+    ].squeeze()  # seq-len(sub) vocab.
+    word_pred_scores_all, word_predictions = torch.topk(
+        word_predictions, k, -1
+    )  # seq-len k  #top k prediction
 
-    word_predictions = word_predictions[1:len(sub_words) + 1, :]
-    word_pred_scores_all = word_pred_scores_all[1:len(sub_words) + 1, :]
+    word_predictions = word_predictions[1 : len(sub_words) + 1, :]
+    word_pred_scores_all = word_pred_scores_all[1 : len(sub_words) + 1, :]
 
-    important_scores = get_important_scores(words, tgt_model, current_prob, orig_label, orig_probs,  #get important score 
-                                            tokenizer, batch_size, max_length)
+    important_scores = get_important_scores(
+        words,
+        tgt_model,
+        current_prob,
+        orig_label,
+        orig_probs,  # get important score
+        tokenizer,
+        batch_size,
+        max_length,
+    )
     feature.query += int(len(words))
-    #sort by important score
-    list_of_index = sorted(enumerate(important_scores), key=lambda x: x[1], reverse=True) # sort the important score and index 
-    # print(list_of_index)  
+    # sort by important score
+    list_of_index = sorted(
+        enumerate(important_scores), key=lambda x: x[1], reverse=True
+    )  # sort the important score and index
+    # print(list_of_index)
     # => [(59, 0.00014871359), (58, 0.00011396408), (60, 0.00010085106), .... ]      [(index, Importacne score), ....]
-    final_words = copy.deepcopy(words) #whole word corpus from mlm 
+    final_words = copy.deepcopy(words)  # whole word corpus from mlm
 
-     
     for top_index in list_of_index:
-        #limit ratio of word change
+        # limit ratio of word change
         if feature.change > int(0.4 * (len(words))):
             feature.success = 1  # exceed
             return feature
@@ -134,12 +176,21 @@ def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=5
         if keys[top_index[0]][0] > max_length - 2:
             continue
 
+        substitutes = word_predictions[
+            keys[top_index[0]][0] : keys[top_index[0]][1]
+        ]  # L, k
+        word_pred_scores = word_pred_scores_all[
+            keys[top_index[0]][0] : keys[top_index[0]][1]
+        ]
 
-        substitutes = word_predictions[keys[top_index[0]][0]:keys[top_index[0]][1]]  # L, k
-        word_pred_scores = word_pred_scores_all[keys[top_index[0]][0]:keys[top_index[0]][1]]
-
-        substitutes = get_substitues(substitutes, tokenizer, mlm_model, use_bpe, word_pred_scores, threshold_pred_score)
-
+        substitutes = get_substitues(
+            substitutes,
+            tokenizer,
+            mlm_model,
+            use_bpe,
+            word_pred_scores,
+            threshold_pred_score,
+        )
 
         most_gap = 0.0
         candidate = None
@@ -149,7 +200,7 @@ def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=5
 
             if substitute == tgt_word:
                 continue  # filter out original word
-            if '##' in substitute:
+            if "##" in substitute:
                 continue  # filter out sub-word
 
             if substitute in filter_words:
@@ -157,26 +208,31 @@ def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=5
             if substitute in w2i and tgt_word in w2i:
                 if cos_mat[w2i[substitute]][w2i[tgt_word]] < 0.4:
                     continue
-            
-            #Replace word and check whether the attack is successful
+
+            # Replace word and check whether the attack is successful
             temp_replace = final_words
-            temp_replace[top_index[0]] = substitute # replace token
+            temp_replace[top_index[0]] = substitute  # replace token
             temp_text = tokenizer.convert_tokens_to_string(temp_replace)
-            inputs = tokenizer.encode_plus(temp_text, None, add_special_tokens=True, max_length=max_length, )
-            input_ids = torch.tensor(inputs["input_ids"]).unsqueeze(0).to('cuda')
+            inputs = tokenizer.encode_plus(
+                temp_text,
+                None,
+                add_special_tokens=True,
+                max_length=max_length,
+            )
+            input_ids = torch.tensor(inputs["input_ids"]).unsqueeze(0).to("cuda")
             seq_len = input_ids.size(1)
             temp_prob = tgt_model(input_ids)[0].squeeze()
             feature.query += 1
             temp_prob = torch.softmax(temp_prob, -1)
             temp_label = torch.argmax(temp_prob)
-            #Success
+            # Success
             if temp_label != orig_label:
                 feature.change += 1
                 final_words[top_index[0]] = substitute
                 feature.changes.append([keys[top_index[0]][0], substitute, tgt_word])
                 feature.final_adverse = temp_text
                 feature.success = 4
-                
+
                 return feature
             else:
 
@@ -192,23 +248,38 @@ def attack(feature, tgt_model, mlm_model, tokenizer, k, batch_size, max_length=5
             current_prob = current_prob - most_gap
             final_words[top_index[0]] = candidate
 
-    feature.final_adverse = (tokenizer.convert_tokens_to_string(final_words))
+    feature.final_adverse = tokenizer.convert_tokens_to_string(final_words)
     feature.success = 2
     return feature
+
+
+target_features = processor.get_dev_data(args.data_dir)
+
 
 with torch.no_grad():
     for index, feature in enumerate(features[start:end]):
         seq_a, label = feature
         feat = Feature(seq_a, label)
-        print('\r number {:d} '.format(index) + tgt_path, end='')
+        print("\r number {:d} ".format(index) + tgt_path, end="")
         # print(feat.seq[:100], feat.label)
-        feat = attack(feat, tgt_model, mlm_model, tokenizer, k, batch_size=32, max_length=512,
-                        cos_mat=cos_mat, w2i=w2i, i2w=i2w, use_bpe=use_bpe,threshold_pred_score=threshold_pred_score)
-        
+        feat = attack(
+            feat,
+            tgt_model,
+            mlm_model,
+            tokenizer,
+            k,
+            batch_size=32,
+            max_length=512,
+            cos_mat=cos_mat,
+            w2i=w2i,
+            i2w=i2w,
+            use_bpe=use_bpe,
+            threshold_pred_score=threshold_pred_score,
+        )
+
         print(feat.changes, feat.change, feat.query, feat.success)
         if feat.success > 2:
-            print('success', end='')
-        else: 
-            print('failed', end='')
+            print("success", end="")
+        else:
+            print("failed", end="")
         features_output.append(feat)
-
